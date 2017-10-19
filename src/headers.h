c82c0c6b500b2426616d392934fea9abc2d97b61
Guilherme Torres <guilhermetorres97@gmail.com>
2017-10-19 11:14:16 -0200

    first set up

---
+++ src/headers.h
@@ -0,0 +1,10 @@
+#ifndef HEADERS_H_
+#define HEADERS_H_
+
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "headers.h"
+
+int main(int argc, char**argv)
+{
+	char* DataFileName = argv[1];
+	cout<<"boop"<<endl;
+}
