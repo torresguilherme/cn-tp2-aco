c4f10c6a2dcb29447605808f94e1eae8eae53376
Guilherme Torres <guilhermetorres97@gmail.com>
2017-11-07 17:07:00 -0200

    pronto para os logs

--- src/main.cpp
+++ src/main.cpp
@@ -21,31 +21,57 @@ int main(int argc, char**argv)
 	// treinamento
 	int ant_number = train_data.get_vertex_number() - train_data.get_p();
 	int ITERATIONS = ant_number * 5;
-	Ant ants[ant_number];
-	double best_solution = DBL_MAX;
+	double bestofall = DBL_MAX;
+	vector<double> best_averages;
+	vector<double> average_averages;
+
 	for(int i = 0; i < ITERATIONS; i++)
 	{
-		for(int j = 0; j < ant_number; j++)
-		{
-			ants[j].make_path(train_data);
-		}
-
-		train_data.decay();
-		double local_fitness = get_fitness(train_data, ants, ant_number, PENALTY_MODIFIER, best_solution);
-		if(local_fitness < best_solution)
-		{
-			best_solution = local_fitness;
-		}
-
-		train_data.reset_medians();
-
-		for(int j = 0; j < ant_number; j++)
-		{
-			ants[j].reset();
-		}
-
-		cout<<best_solution<<endl;
+		best_averages.push_back(0);
+		average_averages.push_back(0);
 	}
+
+	for(int k = 0; k < 30; k++)
+	{
+		Ant ants[ant_number];
+		double best_solution = DBL_MAX;
+		double average = 0;
+		for(int i = 0; i < ITERATIONS; i++)
+		{
+			for(int j = 0; j < ant_number; j++)
+			{
+				ants[j].make_path(train_data);
+			}
+
+			double local_fitness = get_fitness(train_data, ants, ant_number, PENALTY_MODIFIER, best_solution);
+			average = (double)(average * (i) + local_fitness)/(i+1);
+			if(local_fitness < best_solution)
+			{
+				best_solution = local_fitness;
+			}
+
+			train_data.reset_medians();
+
+			for(int j = 0; j < ant_number; j++)
+			{
+				ants[j].reset();
+			}
+
+			best_averages[i] = (double)(best_averages[i] * (k) + best_solution)/(k+1);
+			average_averages[i] = (double)(average_averages[i] * (k) + average)/(k+1);
+		}	
+		
+		train_data.full_reset();
+		if(best_solution < bestofall)
+			bestofall = best_solution;
+	}
+
+	for(int i = 0; i < ITERATIONS; i++)
+	{
+		cout<<i<<' '<<average_averages[i]<<' '<<best_averages[i]<<endl;
+	}
+
+	cout<<bestofall<<endl;
 
 	return 0;
 }
